0a3af3f5e264f5eaa6002c8243d424041f1c3aaf
JuanBernardo <uribejuanb@javeriana.edu.co>
2026-06-02 01:58:28 -0500

    fix: no estaba pasando fit_transform, tenia fit 2 horas desperdiciadas

--- src/data/preprocessor.py
+++ src/data/preprocessor.py
@@ -1,4 +1,6 @@
-
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
 
 def codificar_columna(dataframe, columna, diccionario, tipo) :
     dataframe_copia = dataframe.copy()
@@ -7,3 +9,20 @@ def codificar_columna(dataframe, columna, diccionario, tipo) :
 
     return dataframe_copia
 
+def one_hot_cambio(x_train, x_validar, x_test, categoricas, numericas) :
+    one_hot = OneHotEncoder(handle_unknown = "infrequent_if_exist", sparse_output= True)
+
+    escalador_numerico = StandardScaler()
+
+    procesador = ColumnTransformer(
+        transformers=[
+            ("categorico", one_hot, categoricas),
+            ("numerico", escalador_numerico, numericas)
+        ]
+    )
+
+    x_train_procesado = procesador.fit_transform(x_train)
+    x_validar_procesado = procesador.transform(x_validar)
+    x_test_procesado = procesador.transform(x_test)
+
+    return x_train_procesado, x_validar_procesado, x_test_procesado
